e33fa0174c6e7bed48cc1d9d8bd8021968c75ac4
Teju <bhanu1983@gmail.com>
2025-12-21 19:14:37 +0530

    first commit

--- BlogProject/Routes/userR.js
+++ BlogProject/Routes/userR.js
@@ -1,14 +1,13 @@
 
 const express=require("express")
 const { Register, RegisterGet, login } = require("../Controller/userC")
+const { LoginPage } = require("../../Blog-Login/Controller/userC")
 const U_router=express.Router()
 
 U_router.post("/register",Register)
 U_router.get("/",RegisterGet)
 U_router.get("/register",RegisterGet)
 U_router.post("/login",login)
-U_router.get("/login",(req,res)=>{
-    res.render("login")
-})
+U_router.get("/login",LoginPage)
 
 module.exports=U_router
